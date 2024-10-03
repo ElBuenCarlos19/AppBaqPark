cf5de14f6e3b097194934f48cbc4a11b83a11d9e
DevFajardo <devfajardocadena@gmail.com>
2024-10-02 21:05:25 -0500

    integracion de ubicacion del usuario y solicitud de persmisos de localizacion

--- src/app/screen/_layout.tsx
+++ src/app/screen/_layout.tsx
@@ -1,6 +1,4 @@
 import TabsBar from "../_components/TabsBar";
-import AccountButton from "../_components/AccountButton";
-import { View, Text, StyleSheet } from "react-native";
 
 export default function Layout() {
   return (
