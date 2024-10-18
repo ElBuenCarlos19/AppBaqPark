a325e1cc17067b091002e8c1dffcfc3302d549e5
AlejoCant <alejandrocantillop.ilusa@gmail.com>
2024-10-17 23:41:32 -0500

    app.config.js para eas

---
+++ app.config.js
@@ -0,0 +1,40 @@
+export default {
+    expo: {
+      name: "AppBaqPark",
+      slug: "AppBaqPark",
+      scheme: "acme",
+      userInterfaceStyle: "automatic",
+      orientation: "default",
+      web: {
+      "output": "static",
+      "bundler": "metro"
+        },
+    plugins: [
+      [
+        "expo-router",
+        {
+          "origin": "https://n"
+        }
+      ]
+    ],
+      android: {
+        package: "com.appbaqpark",
+        config: {
+          googleMaps: {
+            apiKey: process.env.GOOGLE_MAPS_API_KEY,
+          },
+        },
+      },
+      extra: {
+        GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+        PARKS_KEY: process.env.PARKS_KEY,
+        eas: {
+          projectId: "bd34d188-c154-409b-8314-797e6d2ee458",
+          secrets: {
+            GOOGLE_MAPS_API_KEY: true,
+            PARKS_KEY: true,
+          },
+        },
+      },
+    },
+  };
